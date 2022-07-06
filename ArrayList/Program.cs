b333faf6e0eb91fb9cac3fd29dadd04fc8c3b18b
istiaqhaider8 <istiaqhaider8@gmail.com>
2022-07-06 15:03:06 +0600

    List And ArrayList

---
+++ ArrayList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Istiaq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList myList = new ArrayList();
+
+            //myList.Add(100);
+            //myList.Add(300);
+            //myList.Add("English");
+
+            List<int> myList = new List<int>();
+
+            myList.Add(199);
+            myList.Add(299);
+            myList.Add(299);
+            myList.Add(299);
+            myList.Add(299);
+            myList.Add(299);
+
+            foreach(object myNumbers in myList)
+            {
+                Console.WriteLine(myNumbers);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
